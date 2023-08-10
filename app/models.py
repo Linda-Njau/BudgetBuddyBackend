38e379b9b0a9a6f9d5392ac91429b862b2deae89
Linda-Njau <lindanjau21@gmail.com>
2023-08-10 12:33:58 +0300

    User service added

--- app/models.py
+++ app/models.py
@@ -1,14 +1,23 @@
-from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 class User(db.Model):
     """User object."""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     
     emails = db.relationship('Email', backref='user', lazy=True)
     payments = db.relationship('Payment', backref='user', lazy=True)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 class Email(db.Model):
     """Email object."""
