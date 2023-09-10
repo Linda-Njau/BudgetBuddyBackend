ec41108315d4b69d0d0b33e2d972528cff5b38e4
Linda-Njau <lindanjau21@gmail.com>
2023-09-10 19:32:11 +0300

    In progress of updating payment_entry new field updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from .config import Config
 from os import path
 
 
 db = SQLAlchemy()
-migrate = Migrate()
+from .models import *
+
+
 DB_NAME = "database.db"
 def create_app(environment: str = 'development'):
     """Create a new flask application"""
@@ -14,25 +15,23 @@ def create_app(environment: str = 'development'):
     environment_config = Config[environment]
     app.config.from_object(environment_config)
     db.init_app(app)
-    migrate.init_app(app, db)
     
     from .api.users import users
     from .api.payment_entry import payment_entries
-    from .api.payment_category import payment_categories
     
     app.register_blueprint(users)
     app.register_blueprint(payment_entries)
-    app.register_blueprint(payment_categories)
+    # app.register_blueprint(payment_categories)
     
     @app.before_request
     def before_request():
         print("Requested endpoint:", request.endpoint)
-
-
-    with app.app_context():
-        create_database(app)
-    
+     
+    if environment == 'testing':
+        with app.app_context():
+            create_database(app)
     return app
+
 def create_database(app):
     """Create a database if it doesn't already exist"""
     if not path.exists('unusual_spending/' + DB_NAME):
