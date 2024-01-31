b9881ca161f84c0a017c5a61635a06ebba5b9c5a
Linda-Njau <lindanjau21@gmail.com>
2024-01-31 14:07:25 +0300

    Preparing to deploy

--- api/run.py
+++ api/run.py
@@ -1,3 +1,4 @@
+import os
 from functools import partial
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
@@ -9,9 +10,9 @@ partial_func = partial(scheduled_budget_check, app=app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
-
+port = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
     scheduler.add_job(id='Scheduled Task', func=partial_func,
                       trigger='interval', minutes=1)
-    app.run(debug=True)
+    app.run(debug=True, port=port)
